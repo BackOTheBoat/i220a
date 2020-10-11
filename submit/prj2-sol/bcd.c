58defc4c7aaab2afce4de8e2bbd9371ac19bd440
Eric Stern <estern7@cs.binghamton.edu>
2020-10-11 18:55:40 -0400

    PJ2 Fixing Errors :(

--- submit/prj2-sol/bcd.c
+++ submit/prj2-sol/bcd.c
@@ -12,7 +12,7 @@
    *
    *  EX: getHexSegment(1944, 4) = 9 
    *  EX: getHexSegment(1944, 0) = 8 
-   *\/ 
+   */ 
   int getHexSegment(long long value, int startPos) 
   { 
     if (startPos < 0) 
@@ -36,12 +36,12 @@
   } 
 
 
-  /\** Returns a digit in hexadecimal at place INDEX of a VALUE in base 10 
+  /** Returns a digit in hexadecimal at place INDEX of a VALUE in base 10 
    * 
    *  EX: getHexDigit(1944, 0) = 8 
    *  EX: getHexDigit(1944, 1) = 9 
    *  EX: getHexDigit(1944, 2) = 7 
-   *\/ 
+   */ 
   int getHexDigit(long long value, int index) 
   { 
     int hexDigits = 1; 
@@ -70,10 +70,10 @@
     return digit; 
   } 
 
-  /\** Returns the value of a given index in BCD 
+  /** Returns the value of a given index in BCD 
    * 
    * CHECK: Index >= 0 
-   *\/ 
+   */ 
   int getBcdDigit(Bcd bcd, int index) 
   { 
     if (index < 0) 
@@ -94,11 +94,11 @@
     return digit; 
   } 
 
-  /\** Returns a BCD with the given INDEX changed to VALUE 
+  /** Returns a BCD with the given INDEX changed to VALUE 
    * 
    * CHECK: Index >= 0 
    * CHECK: Value >= 0 
-   *\/ 
+   */ 
   Bcd setBcdDigit(Bcd bcd, int index, int value) 
   { 
     if (index < 0) 
@@ -179,14 +179,14 @@
     return result; 
   } 
 
-  /\** Return BCD encoding of binary (which has normal binary representation). 
+  /** Return BCD encoding of binary (which has normal binary representation). 
    * 
    *  Examples: binary_to_bcd(0xc) => 0x12; 
    *            binary_to_bcd(0xff) => 0x255 
    * 
    *  If error is not NULL, sets *error to OVERFLOW_ERR if binary is too 
    *  big for the Bcd type, otherwise *error is unchanged. 
-   *\/ 
+   */ 
   Bcd binary_to_bcd(Binary value, BcdError *error) 
   { 
     //@TODO -- DONE 
@@ -229,7 +229,7 @@
     return result; 
   } 
 
-  /\** Return binary encoding of BCD value bcd. 
+  /** Return binary encoding of BCD value bcd. 
    * 
    *  Examples: bcd_to_binary(0x12) => 0xc; 
    *            bcd_to_binary(0x255) => 0xff 
@@ -237,7 +237,7 @@
    *  If error is not NULL, sets *error to BAD_VALUE_ERR if bcd contains 
    *  a bad BCD digit. 
    *  Cannot overflow since Binary can represent larger values than Bcd 
-   *\/ 
+   */ 
   Binary bcd_to_binary(Bcd bcd, BcdError *error) 
   { 
     //@TODO -- DONE 
@@ -260,14 +260,14 @@
     return result; 
   } 
 
-  /\** Return BCD encoding of decimal number corresponding to string s. 
+  /** Return BCD encoding of decimal number corresponding to string s. 
    *  Behavior undefined on overflow or if s contains a non-digit 
    *  character.  Sets *p to point to first non-digit char in s. 
    *  Rougly equivalent to strtol(). 
    * 
    *  If error is not NULL, sets *error to OVERFLOW_ERR if binary is too 
    *  big for the Bcd type, otherwise *error is unchanged. 
-   *\/ 
+   */ 
   Bcd str_to_bcd(const char *s, const char **p, BcdError *error) 
   { 
     //@TODO 
@@ -292,8 +292,7 @@
     } 
 
     return binary_to_bcd(value, &error); 
-  
-    /\* 
+    /**
     Binary number = 0; 
     Bcd result = 0; 
     for(char *x = s; *x != '\0'; x++) 
@@ -306,18 +305,17 @@
       } 
       else 
       { 
-        //\**p = &s; 
+        //p = &s; 
       } 
     } 
     //printf("Number is: %d\n", number); 
     result = binary_to_bcd(number, &error); 
   
-    return result; 
-    *\/ 
+    return result; */
     //The above code was removed in favor of a shorter approach 
   } 
 
-  /\** Convert bcd to a NUL-terminated string in buf[] without any 
+  /** Convert bcd to a NUL-terminated string in buf[] without any 
    *  non-significant leading zeros.  Never write more than bufSize 
    *  characters into buf.  The return value is the number of characters 
    *  written (excluding the NUL character used to terminate strings). 
@@ -325,19 +323,19 @@
    *  If error is not NULL, sets *error to BAD_VALUE_ERR is bcd contains 
    *  a BCD digit which is greater than 9, OVERFLOW_ERR if bufSize bytes 
    *  is less than BCD_BUF_SIZE, otherwise *error is unchanged. 
-   *\/ 
+   */ 
   int bcd_to_str(Bcd bcd, char buf[], size_t bufSize, BcdError *error) 
   { 
     //@TODO 
     return 0; 
   } 
 
-  /\** Return the BCD representation of the sum of BCD int's x and y. 
+  /** Return the BCD representation of the sum of BCD int's x and y. 
    * 
    *  If error is not NULL, sets *error to to BAD_VALUE_ERR is x or y 
    *  contains a BCD digit which is greater than 9, OVERFLOW_ERR on 
    *  overflow, otherwise *error is unchanged. 
-   *\/ 
+   */ 
   Bcd bcd_add(Bcd x, Bcd y, BcdError *error) 
   { 
     //@TODO 
@@ -402,12 +400,12 @@
     return result; 
   } 
 
-  /\** Return the BCD representation of the product of BCD int's x and y. 
+  /** Return the BCD representation of the product of BCD int's x and y. 
    * 
    * If error is not NULL, sets *error to to BAD_VALUE_ERR is x or y 
    * contains a BCD digit which is greater than 9, OVERFLOW_ERR on 
    * overflow, otherwise *error is unchanged. 
-   *\/ 
+   */ 
   Bcd bcd_multiply(Bcd x, Bcd y, BcdError *error) 
   { 
     //@TODO 
